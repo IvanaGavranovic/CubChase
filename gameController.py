f799f66b3f1851b6c3f655e8847822d6a80eec11
IvanaGavranovic <ivanagavranovic@outlook.com>
2020-01-14 00:50:31 +0100

    refactoring

--- gameController.py
+++ gameController.py
@@ -1,22 +1,22 @@
+from board import Board
 from timon import *
 from pumba import *
-from threading import Thread
+from trap import *
+from pictures import *
 
 
 class GameController:
 
-    def __init__(self):
+    def __init__(self, lock_object=None):
         self.board = Board()
-        # ovde puca (line 11)
-        self.timon = Timon(1, 1, TIMON_YELLOW, self.board)
-        self.pumba = Pumba(18, 14, PUMBA_YELLOW, self.board)
+        self.timon = Timon(1, 1, TIMON_YELLOW, self.board, lock_object)
+        self.pumba = Pumba(18, 14, PUMBA_YELLOW, self.board, lock_object)
+        self.trap = Trap(1, 4, TRAP_PASSIV, self.board)
         self.board.set_field(1, 1, TIMON_YELLOW)
         self.board.set_field(18, 14, PUMBA_YELLOW)
 
     def timon_movement(self):
         self.timon.changePosition()
-        #self.timon.changePositionTimon()
 
     def pumba_movement(self):
         self.pumba.changePosition()
-        #self.pumba.changePositionPumba()

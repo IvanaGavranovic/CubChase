be043e1a98320a6c124d3c4d452ebca9ddb57ae7
IvanaGavranovic <ivanagavranovic@outlook.com>
2020-01-15 06:12:16 +0100

    corrected class trap

--- trap.py
+++ trap.py
@@ -5,10 +5,11 @@ from pictures import *
 
 class Trap(QLabel):
 
+    Trap = 1
     isActive = False
     activeTrap = pyqtSignal()
 
-    def __init__(self, x, y, picture, board, t):
+    def __init__(self, x, y, picture, board, id, t):
         super().__init__()
         self.board = board
         self.initTrap(x, y, picture, id, t)
@@ -16,22 +17,19 @@ class Trap(QLabel):
     def initTrap(self, x, y, picture, id, t):
         self.X = x
         self.Y = y
-        self.ID = id
         self.Picture = picture
-        self.activeTrap.connect(self.active)
+        self.ID = id
         self.Trap = t
-        self.Label = QLabel()
-        self.Label.move(x, y)
-
+        self.activeTrap.connect(self.active)
 
     def trap(self, enemy):
         print("")
 
     def active(self):
         self.isActive= True
-        self.picture(TRAP_ACTIVE)
+        self.Picture = TRAP_ACTIVE
         loop = QEventLoop()
         QTimer.singleShot(10000, loop.quit)
         loop.exec_()
-        self.picture(TRAP_PASSIVE)
+        self.Picture = TRAP_PASSIVE
         self.isActive = False

ad1a897a5948922cb8fe369803a54f5c9b21002b
nenads97 <nenad.suknovicpr143@gmail.com>
2020-01-16 22:44:23 +0100

    Update gameController
    Dodao neophodne stavke za simbu

--- gameController.py
+++ gameController.py
@@ -1,19 +1,25 @@
 from board import Board
 from timon import *
 from pumba import *
+from simba import *
 from trap import *
 from pictures import *
 
 
 class GameController:
 
-    def __init__(self, lock_object=None):
+    def __init__(self, lock_object=None, key = None):
         self.board = Board()
         self.timon = Timon(1, 1, TIMON_YELLOW, self.board, lock_object)
         self.pumba = Pumba(18, 14, PUMBA_YELLOW, self.board, lock_object)
+        self.simba = Simba(1, 2, SIMBA_YELLOW, self.board, lock_object, key)
         self.trap = Trap(4, 2, TRAP_PASSIVE, self.board, 1, 1)
         self.board.set_field(1, 1, TIMON_YELLOW)
         self.board.set_field(18, 14, PUMBA_YELLOW)
+        self.board.set_field(1, 2, SIMBA_YELLOW)
+
+    def simba_movement(self):
+        self.simba.changePosition()
 
     def timon_movement(self):
         self.timon.changePosition()
